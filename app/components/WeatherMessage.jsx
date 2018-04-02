16c54ef975c5d183f2451358f04d87646ecf562d
Peter <peter.fabian@pintofsource.com>
2018-04-02 22:04:23 +0200

    styling weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react')
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>I'ts {temp} in {location}</h3>
+    <h3 className="text-center">I'ts {temp} in {location}</h3>
   )
 }
 
